08ed51d32a43c0e3471aa0d9c82cab74005819af
Void <trigger_austin00@proton.me>
2024-05-18 16:11:30 +0300

    called new from storage removed self.save call in innit and new from storage in save of basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self.save()
+            self._import_storage().new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -31,7 +31,6 @@ class BaseModel:
             Updates updated_at with the current datetime and saves the instance
         """
         self.updated_at = datetime.now()
-        self._import_storage().new(self)
         self._import_storage().save()
 
     def to_dict(self):

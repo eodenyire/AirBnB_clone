d49e51fffd9d35dbb979bb1dab101f20602e922a
Void <trigger_austin00@proton.me>
2024-05-18 15:52:58 +0300

    fixed updated_at generation

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             self.save()
 
     def __str__(self):
@@ -31,9 +31,8 @@ class BaseModel:
             Updates updated_at with the current datetime and saves the instance
         """
         self.updated_at = datetime.now()
-        from models import storage
-        storage.new(self)
-        storage.save()
+        self._import_storage().new(self)
+        self._import_storage().save()
 
     def to_dict(self):
         """Returns a dictionary representation of the instance"""
@@ -42,3 +41,7 @@ class BaseModel:
         dict_representation['created_at'] = self.created_at.isoformat()
         dict_representation['updated_at'] = self.updated_at.isoformat()
         return dict_representation
+
+    def _import_storage(self):
+        from models import storage
+        return storage

470e7ac56a4eb12691078397fdde016129ad1723
Void <trigger_austin00@proton.me>
2024-05-18 17:02:01 +0300

    edited new

--- models/base_model.py
+++ models/base_model.py
@@ -1,64 +1,46 @@
 #!/usr/bin/python3
-"""
-This module defines all common attributes/methods for other classes
-"""
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
-    """The Base class"""
+    """Defines all common attributes and methods for all other classes"""
+
     def __init__(self, *args, **kwargs):
-        """constructor of a base model"""
+        """Initializes an instance of BaseModel"""
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if kwargs.get("created_at",
-                          None) and isinstance(self.created_at, str):
-                self.created_at = datetime.strptime(kwargs["created_at"],
-                                                    '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at",
-                          None) and isinstance(self.updated_at, str):
-                self.updated_at = datetime.strptime(kwargs["updated_at"],
-                                                    '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                self.updated_at = datetime.utcnow()
-            if "id" not in kwargs:
-                self.id = str(uuid.uuid4())
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at
-            models.storage.new(self)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            self._import_storage().new(self)
 
     def __str__(self):
-        """print formatted string"""
-        return '[{}] ({}) {}'.format(self.__class__.__name__,
-                                     self.id,
-                                     self.__dict__)
+        """Returns a string representation of the instance"""
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """updates the public instance attribute
-        updated_at with the current datetime"""
+        """
+            Updates updated_at with the current datetime and saves the instance
+        """
         self.updated_at = datetime.now()
-        models.storage.save()
+        self._import_storage().save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values
-        of __dict__ of the instance"""
-        my_dict = {
-            "__class__": self.__class__.__name__
-        }
-        # update with instance attributes stored under the hood
-        my_dict.update(self.__dict__)
+        """Returns a dictionary representation of the instance"""
+        dict_representation = self.__dict__.copy()
+        dict_representation['__class__'] = self.__class__.__name__
+        dict_representation['created_at'] = self.created_at.isoformat()
+        dict_representation['updated_at'] = self.updated_at.isoformat()
+        return dict_representation
 
-        if "created_at" in my_dict:
-            my_dict["created_at"] = self.created_at.isoformat()
-        if "updated_at" in my_dict:
-            my_dict["updated_at"] = self.updated_at.isoformat()
-        return my_dict
-
+    def _import_storage(self):
+        from models import storage
+        return storage

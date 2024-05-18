eb6a44664d9386675fc117be3b6149e2c9f157c2
Void <trigger_austin00@proton.me>
2024-05-18 16:35:03 +0300

    Revert "edited filestorage reload"
    This reverts commit 6ec5be248fe355e1ceb9e348825c91104bc1927e.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -52,11 +52,9 @@ class FileStorage:
         try:
             with open(self.__file_path, 'r')as file:
                 json_obj = json.load(file)
-                for key, value in json_obj:
-                    cls_name = value["__class__"]
-                    cls = self.get_attr_name_from_classes(cls_name)
-                    if cls:
-                        self.__objects[key] = cls(**value)
+                for key in json_obj:
+                    cls_name = json_obj[key]["__class__"]
+                    self.__objects[key] = eval(cls_name)(**json_obj[key])
         except FileNotFoundError:
             pass
 

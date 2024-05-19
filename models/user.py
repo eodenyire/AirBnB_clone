fec8bbb23adebaf4e9dcc3e4439c85fa71e144a2
Void <trigger_austin00@proton.me>
2024-05-20 00:45:13 +0300

    added __init__ method

--- models/user.py
+++ models/user.py
@@ -3,7 +3,22 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """
+        defines a class User that inherits from BaseModel
+
+        Attributes:
+            email, password, first_name, last_name
+    """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+            Initializes a new User class
+
+            Args:
+                *args, **kwargs
+        """
+        super().__init__(*args, **kwargs)
